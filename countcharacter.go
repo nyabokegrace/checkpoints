87c93bedcf123d136f2cfd877350a5ebecb4613d
gnyaboke <nyabokegrace007@gmail.com>
2026-05-12 15:26:44 +0300

    changes

--- countcharacter.go
+++ countcharacter.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func CountCharacter(s string) int {
+func CountCharacter(s string, c rune) int {
 	count := 0
 	for _, ch := range s {
-		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9') {
+		if ch == c {
 			count++
 		}
 	}
@@ -13,6 +13,5 @@ func CountCharacter(s string) int {
 }
 
 func main() {
-	fmt.Println(CountCharacter("AEiOu78"))
-	fmt.Println(CountCharacter("grace"))
+	fmt.Println(CountCharacter("graceee", 'e'))
 }
