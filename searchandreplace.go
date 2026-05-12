5b4f00a3ae84cf7675382f6fdbf226015c488e13
gnyaboke <nyabokegrace007@gmail.com>
2026-05-12 18:03:52 +0300

    channges

---
+++ searchandreplace.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		return
+	}
+	str, old, new := os.Args[1], os.Args[2], os.Args[3]
+	for i := 0; i < len(str); i++ {
+		if (len(old) && str[i] == old[0]) && len(old) == 1 {
+			fmt.Println((new[0]))
+		} else {
+			fmt.Println((str[i]))
+		}
+	}
+	fmt.Println("\n")
+}
--- weareunique.go
+++ weareunique.go
@@ -2,29 +2,29 @@ package main
 
 import "fmt"
 
-func contains(s string , c rune) bool{
-	for _, v:= range s {
-		if v==c {
+func contains(s string, c rune) bool {
+	for _, v := range s {
+		if v == c {
 			return true
 		}
 	}
 	return false
 }
 
-func weAreUnique(str1, str2 string)int {
-	if str1=="" && str2=="" {
+func weAreUnique(str1, str2 string) int {
+	if str1 == "" && str2 == "" {
 		return -1
 	}
-	unique:=""
+	unique := ""
 
-	for _,c := range str1 {
+	for _, c := range str1 {
 		if !contains(str2, c) && !contains(unique, c) {
-			unique= unique+ string(c)
+			unique = unique + string(c)
 		}
 	}
-	for _,c := range str2 {
+	for _, c := range str2 {
 		if !contains(str1, c) && !contains(unique, c) {
-			unique= unique+ string(c)
+			unique = unique + string(c)
 		}
 	}
 	return len(unique)
