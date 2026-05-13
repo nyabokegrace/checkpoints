14d48607e34389e1db13f9839e6f8f5efe18302f
gnyaboke <nyabokegrace007@gmail.com>
2026-05-13 08:27:07 +0300

    changes

--- searchandreplace.go
+++ searchandreplace.go
@@ -9,13 +9,13 @@ func main() {
 	if len(os.Args) != 4 {
 		return
 	}
-	str, old, new := os.Args[1], os.Args[2], os.Args[3]
+	str, old, new := os.Args[1], os.Args[2][0], os.Args[3][0]
 	for i := 0; i < len(str); i++ {
-		if i< (len(old) && str[i] == old[0]) && len(old) == 1 {
-			fmt.Println((new[0]))
+		if str[i] == old {
+			fmt.Printf("%c", new)
 		} else {
-			fmt.Println((str[i]))
+			fmt.Printf("%c", str[i])
 		}
 	}
-	fmt.Println("\n")
+	fmt.Println()
 }
