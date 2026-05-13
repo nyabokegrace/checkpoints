baefa7f96116dc0cfcc27252f9735268500aef2a
gnyaboke <nyabokegrace007@gmail.com>
2026-05-13 22:37:50 +0300

    bim

---
+++ ctscase.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func CamelToSnakeCase(s string) string {
+	
+	n:=len(s)
+	result := ""
+	
+
+	for i, v:= range s {
+		last := s[n-1]
+
+		if len(s)== 0 {
+			return s
+		}
+		if !(v >= 'a' && v <= 'z') && !(v >= 'A' && v <= 'Z'){
+			return s
+		}
+		if last >='A' && last <= 'Z' {
+			return s
+		}
+		if i>0 && (v >='A' && v <='Z') && (s[i-1] >= 'A' && s[i-1] <= 'Z') {
+			return s
+		}
+		if i>0 && ( v >= 'A' && v <= 'Z') {
+			result += "_"
+		}
+		result+= string(v)
+	}
+	return result
+}
+
+func main() {
+ fmt.Println(CamelToSnakeCase("CamelCase"))
+}
