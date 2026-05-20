d66b7b57cde2df3fdaf674fdec73873dc85aee20
gnyaboke <nyabokegrace007@gmail.com>
2026-05-20 12:10:31 +0300

    first commit

---
+++ revwstr.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"fmt"
+)
+func main()  {
+	if len(os.Args)!=2{
+		return
+	}
+	s:=os.Args[1]
+	if s==""{
+		fmt.Print("\n")
+		return
+	}
+	w:=[]string{""}
+	for i:=0; i<len(s); i++{
+		if s[i] == ' '{
+			w=append(w, "")
+		}else{
+			w[len(w)-1]+=string(s[i])
+		}
+	}
+	for i:= len(w)-1; i>=0; i--{
+		for _, r:= range w[i]{
+			fmt.Print(string(r))
+		}
+		if i>0{
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("\n")
+}
