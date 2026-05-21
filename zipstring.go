09ccc282a4030d5c7b060e43d95d3b1e593af9a3
michaelochieng0 <mclay@kabarak.ac.ke>
2026-05-21 16:54:37 +0300

    added zipstring to the repo, effecient enough

--- zipstring.go
+++ zipstring.go
@@ -1,26 +1,44 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func ZipString(s string) string {
-	if s == " " {
-		return " "
-	}
-	result := " "
-	count := 1
-	for i := 1; i < len(s); i++ {
-		if s[i] == s[i-1] {
-			count++
-		} else {
-			result += string(rune(count%10+'0')) + string(s[i-1])
-			count /= 10
-			count = 1
-		}
-	}
-	result += string(rune(count+'0')) + string(s[len(s)-1])
-	return result
+    if len(s) == 0 {
+        return ""
+    }
+
+    result := ""
+    count := 1
+
+    for i := 1; i <= len(s); i++ {
+        // trigger flush when reaching end OR character changes
+        if i == len(s) || s[i] != s[i-1] {
+            tmp := count
+            digits := ""
+
+            if tmp == 0 {
+                digits = "0"
+            } else {
+                for tmp > 0 {
+                    digits = string(rune('0'+(tmp%10))) + digits
+                    tmp /= 10
+                }
+            }
+
+            result += digits
+            result += string(s[i-1])
+
+            count = 1
+        } else {
+            count++
+        }
+    }
+
+    return result
 }
 
-func main() {
-	fmt.Println(ZipString("aaaaaaaaaabbbccc"))
+func main()  {
+	fmt.Println(ZipString("aaaaaaaaaaaaaaaqqwedeeetrtrrgghghhyy"))
 }
