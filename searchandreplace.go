8ec3c15bd94aac889c5e6bd0d70bd266514f90e4
nyabokegrace <157786175+nyabokegrace@users.noreply.github.com>
2026-05-21 21:39:21 +0300

    Improve print statements with comments
    Refactor print statements to include comments for clarity.

--- searchandreplace.go
+++ searchandreplace.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-)
+) //for piscine import os only//
 
 func main() {
 	if len(os.Args) != 4 {
 		return
 	}
-
+//for piscine initialize result:=""//
 	str, old, new := os.Args[1], os.Args[2][0], os.Args[3][0]
 	for i := 0; i < len(str); i++ {
 		if str[i] == old {
-			fmt.Printf("%c", new)
+			fmt.Printf("%c", new) //return result+=string(new)//
 		} else {
-			fmt.Printf("%c", str[i])
+			fmt.Printf("%c", str[i])//return result+=string(s[i])//
 		}
 	}
 	fmt.Println()
