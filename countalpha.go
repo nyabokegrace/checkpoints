0f7ac36bd4890cb67d5660a1dc258ac4bbfde97c
gnyaboke <nyabokegrace007@gmail.com>
2026-06-18 16:10:20 +0300

    made some changes to rhe return

--- countalpha.go
+++ countalpha.go
@@ -5,11 +5,12 @@ import "fmt"
 func CountAlpha(s string) int {
 	count := 0
 	for _, ch := range s {
-		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') 
+		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') {
 			count++
 		}
-		return count
 	}
+	return count
+}
 
 func main() {
 	fmt.Println(CountAlpha("grac*#e"))
