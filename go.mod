6a41a864be6d8a44044f762946bcb7bdd7f4d8cc
michaelochieng0 <mclay@kabarak.ac.ke>
2026-09-23 16:12:47 +0300

    search replace

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module rostring.go
 
-go 1.24.3
+go 1.22.2
 
-require github.com/01-edu/z01 v0.1.0 // indirect
+require github.com/01-edu/z01 v0.1.0
--- searchandreplace.go
+++ searchandreplace.go
@@ -1,4 +1,4 @@
-working piscine version with z01.PrintRune 
+//working piscine version with z01.PrintRune 
 
 package main
 
@@ -12,12 +12,12 @@ func main() {
 	if len(os.Args) != 4 {
 		return
 	}
-	str1, str2, str3 := os.Args[1], os.Args[2][0], os.Args[3][0]
-	for i := 0; i < len(str1); i++ {
-		if str1[i] == str2 {
-			z01.PrintRune(rune(str3))
+	str, old, new := os.Args[1], os.Args[2][0], os.Args[3][0]
+	for i := 0; i < len(str); i++ {
+		if str[i] == old {
+			z01.PrintRune(rune(new))
 		} else {
-			z01.PrintRune(rune(str1[i]))
+			z01.PrintRune(rune(str[i]))
 		}
 	}
 	z01.PrintRune('\n')
